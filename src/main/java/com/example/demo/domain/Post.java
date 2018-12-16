512bc6b72843b10b023a73b9344d28b522c74cd0
Jorge Rabadan <jorge96_rm@hotmail.es>
2018-12-17 00:24:09 +0100

    Commit final
    Arreglado el borrar usuario.
    Mensaje de error para toda la página.

--- src/main/java/com/example/demo/domain/Post.java
+++ src/main/java/com/example/demo/domain/Post.java
@@ -1,11 +1,17 @@
 package com.example.demo.domain;
 
+import java.util.Collection;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 //import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
 import com.example.demo.domain.Usuario;
 
@@ -21,6 +27,10 @@ public class Post {
 	private Long nRespuestas;
 	private Long nVisitas;
 	
+	@Cascade(CascadeType.DELETE)
+	@OneToMany(mappedBy="postRespuesta")
+	private Collection<Respuesta> postrespuesta;
+
 	@ManyToOne
 	private Categoria nombre_categoria;
 	
@@ -39,6 +49,14 @@ public class Post {
 	
 	public Post() {
 		super();
+	}
+	
+	public Collection<Respuesta> getPostrespuesta() {
+		return postrespuesta;
+	}
+
+	public void setPostrespuesta(Collection<Respuesta> postrespuesta) {
+		this.postrespuesta = postrespuesta;
 	}
 	
 	public Categoria getNombre_categoria() {
